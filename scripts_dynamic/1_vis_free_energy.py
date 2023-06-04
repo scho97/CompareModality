5f53c88ae6eb95040e38044fb536bcdd75bd90cb
scho97 <sungjun.cho@psych.ox.ac.uk>
2023-06-04 15:58:27 +0100

    fix: allow plotting for best runs

--- scripts_dynamic/1_vis_free_energy.py
+++ scripts_dynamic/1_vis_free_energy.py
@@ -11,7 +11,17 @@ from utils.visualize import plot_free_energy
 
 if __name__ == "__main__":
     # Set hyperparameters
-    runs = np.arange(0, 10) # runs to copmare free energy
+    runs = {
+        "hmm": {
+            "eeg": [6, 15, 25, 32, 42, 50, 62, 77, 84, 93],
+            "meg": [3, 15, 22, 31, 49, 57, 64, 74, 89, 96],
+        },
+        "dynemo": {
+            "eeg": [2, 16, 20, 35, 43, 58, 68, 70, 86, 94],
+            "meg": [0, 14, 21, 30, 49, 58, 62, 77, 82, 93],
+        },
+    } # runs to compare free energy
+    plot_verbose = True
 
     # Set up directories
     BASE_DIR = "/well/woolrich/users/olt015/CompareModality/results/dynamic"
@@ -23,7 +33,10 @@ if __name__ == "__main__":
     for mdl in ["hmm", "dynemo"]:
         print(f"*** Comparison of {mdl.upper()} Free Energy ***")
         for mod, mod_dir in zip(["eeg", "meg"], [LEMON_DIR, CAMCAN_DIR]):
-            run_ids = [f"run{i}_{mdl}" for i in runs]
+            if isinstance(runs, np.ndarray):
+                run_ids = [f"run{i}_{mdl}" for i in runs]
+            elif isinstance(runs, dict):
+                run_ids = [f"run{i}_{mdl}" for i in runs[mdl][mod]]
             print(f"\tGetting results from {mod.upper()} data ...")
             F[mdl][mod] = get_free_energy(mod_dir, run_ids, data_type=mod, model=mdl)              
             # Print the best run
@@ -32,13 +45,15 @@ if __name__ == "__main__":
             print(f"\tThe lowest free energy is {best_F} from {best_run}.")
 
     # Visualize free energy over multiple runs for comparison
-    plot_verbose = True
     if plot_verbose:
+        if isinstance(runs, np.ndarray):
+            savename = f"free_energy_{mod}_{runs[0]}_{runs[-1]}.png"
+        else: savename = f"free_energy_best_runs.png"
         for mod, mod_dir in zip(["eeg", "meg"], [LEMON_DIR, CAMCAN_DIR]):
             plot_free_energy(
                 F, 
                 modality=mod, 
-                filename=os.path.join(mod_dir, f"free_energy_{mod}_{runs[0]}_{runs[-1]}.png")
+                filename=os.path.join(mod_dir, savename),
             )
 
     print("Visualisation complete.")
