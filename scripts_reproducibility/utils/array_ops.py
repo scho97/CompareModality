6d398e8897c81ee8028e978d8fb230b3421e2d4b
scho97 <sungjun.cho@psych.ox.ac.uk>
2023-06-14 13:19:30 +0100

    enhance: take user-defined min, max values

--- scripts_reproducibility/utils/array_ops.py
+++ scripts_reproducibility/utils/array_ops.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-def min_max_scale(input):
+def min_max_scale(input, minimum=None, maximum=None):
     """Normalizes input data from -1 to 1.
 
     Parameters
@@ -13,6 +13,12 @@ def min_max_scale(input):
         Input data to be scaled. If input is a list, each data will
         be scaled by the minimum and maximum value taken from entire 
         data items in the list.
+    minimum : float
+        Minimum value to scale the input data. Defaults to None,
+        which will take a minimum from the input value.
+    maximum : float
+        Maximum value to scale the input data. Defaults to None,
+        which will take a maximum from the input value.
     
     Returns
     -------
@@ -20,15 +26,25 @@ def min_max_scale(input):
         Input data scaled to be between -1 and 1.
     """
     
+    # Validation
+    if not all((minimum is None, maximum is None)):
+        if None in [minimum, maximum]:
+            raise ValueError("Both minimum and maximum values should be provided or set us None.")
+
+    # Compute normalization constant
+    if (minimum is None) and (maximum is None):
+        range = np.nanmax(input) - np.nanmin(input)
+        minimum = np.nanmin(input)
+    else:
+        range = maximum - minimum
+
+    # Scale input
     if isinstance(input, np.ndarray):
-        range = (input.max() - input.min())
-        scaled_input = (input - input.min()) / range
+        scaled_input = (input - minimum) / range
         scaled_input *= 2
         scaled_input -= 1
     
     if isinstance(input, list):
-        range = np.max(input) - np.min(input)
-        minimum = np.min(input)
         scaled_input = [
             (2 * (data - minimum) / range) - 1
             for data in input
